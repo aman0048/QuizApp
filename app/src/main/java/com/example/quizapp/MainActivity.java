2d350f736ab434eaf88089bd23aaffe1839ec458
aman0048 <jerrygarg16@gmail.com>
2021-05-24 15:48:18 +0530

    new changes

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -16,6 +17,16 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
 
+    int count = 0;
+    Button btn,clear;
+    RadioGroup radio1;
+    RadioGroup radio2;
+    RadioGroup radio3;
+    RadioGroup radio4;
+    RadioButton r1;
+    RadioButton r2;
+    RadioButton r3;
+    RadioButton r4;
 
 
     @Override
@@ -23,77 +34,143 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button) findViewById(R.id.submit);
-        RadioGroup radio1 = (RadioGroup) findViewById(R.id.radiogroup1);
-        RadioGroup radio2 = (RadioGroup) findViewById(R.id.radiogroup2);
-        RadioGroup radio3 = (RadioGroup) findViewById(R.id.radiogroup3);
-        RadioGroup radio4 = (RadioGroup) findViewById(R.id.radiogroup4);
+        btn = (Button) findViewById(R.id.submit);
+        clear = (Button)findViewById(R.id.clear);
+        radio1 = (RadioGroup) findViewById(R.id.radiogroup1);
+        radio2 = (RadioGroup) findViewById(R.id.radiogroup2);
+        radio3 = (RadioGroup) findViewById(R.id.radiogroup3);
+        radio4 = (RadioGroup) findViewById(R.id.radiogroup4);
 
+        radio1.clearCheck();
+        radio2.clearCheck();
+        radio3.clearCheck();
+        radio4.clearCheck();
 
+        radio1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-        LinearLayout layout1 = (LinearLayout) findViewById(R.id.linearlayout1); 
+                // Get the selected Radio Button
+                RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
+            }
+        });
+        radio2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                // Get the selected Radio Button
+                RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
+            }
+        });
+        radio3.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                // Get the selected Radio Button
+                RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
+            }
+        });
+        radio4.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                // Get the selected Radio Button
+                RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
+            }
+        });
+
+//        LinearLayout layout1 = (LinearLayout) findViewById(R.id.linearlayout1);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                int count = 0;
                 int ans1 = radio1.getCheckedRadioButtonId();
                 int ans2 = radio2.getCheckedRadioButtonId();
                 int ans3 = radio3.getCheckedRadioButtonId();
                 int ans4 = radio4.getCheckedRadioButtonId();
 
-                RadioButton r1 = (RadioButton) findViewById(ans1);
-                RadioButton r2 = (RadioButton) findViewById(ans2);
-                RadioButton r3 = (RadioButton) findViewById(ans3);
-                RadioButton r4 = (RadioButton) findViewById(ans4);
+                r1 = (RadioButton) findViewById(ans1);
+                r2 = (RadioButton) findViewById(ans2);
+                r3 = (RadioButton) findViewById(ans3);
+                r4 = (RadioButton) findViewById(ans4);
 //                String s1 = r1.getText().toString();
-                String s1 = (String) r1.getText();
-                String s2 = (String) r2.getText();
-                String s3 = (String) r3.getText();
-                String s4 = (String) r4.getText();
+                String s1 = r1.getText().toString();
+                String s2 = r2.getText().toString();
+                String s3 = r3.getText().toString();
+                String s4 = r4.getText().toString();
                 String correctAns1 = "mucormycosis";
                 String correctAns2 = "3.14";
                 String correctAns3 = "yes";
                 String correctAns4 = "100A/B";
 
-//                if (s1.equals(correctAns1)) {
+                if (s1.contentEquals(correctAns1)) {
+                    count+=1;
+                }
+
+                if (s2.contentEquals(correctAns2)) {
+                    count++;
+                }
+                if (s3.contentEquals(correctAns3)) {
+                    count++;
+                }
+                if (s4.contentEquals(correctAns4)) {
+                    count++;
+                }
+                //waise toh isse se ghamta farak nhi padna chaiye but still i am doing;
+
+//                if (ans1 == -1) {
+//                    Toast.makeText(MainActivity.this, "No answer has been selected", Toast.LENGTH_SHORT).show();
+//                } else {
+//
+//                    RadioButton radioButton = (RadioButton) radio1.findViewById(ans1);
+//
+//                    Toast.makeText(MainActivity.this, radioButton.getText(), Toast.LENGTH_SHORT).show();
 //                    count++;
 //                }
 //
-//                if (s2.equals(correctAns2)) {
+//                if (ans2 == -1) {
+//                    Toast.makeText(MainActivity.this, "No answer has been selected", Toast.LENGTH_SHORT).show();
+//                } else {
+//
+//                    RadioButton radioButton = (RadioButton) radio2.findViewById(ans2);
+//
+//                    Toast.makeText(MainActivity.this, radioButton.getText(), Toast.LENGTH_SHORT).show();
 //                    count++;
 //                }
-//                if (s3.equals(correctAns3)) {
+//
+//                if (ans3 == -1) {
+//                    Toast.makeText(MainActivity.this, "No answer has been selected", Toast.LENGTH_SHORT).show();
+//
+//                } else {
+//
+//                    RadioButton radioButton = (RadioButton) radio3.findViewById(ans3);
+//
+//                    Toast.makeText(MainActivity.this, radioButton.getText(), Toast.LENGTH_SHORT).show();
 //                    count++;
 //                }
-//                if (s4.equals(correctAns4)) {
+//
+//                if (ans4 == -1) {
+//                    Toast.makeText(MainActivity.this, "No answer has been selected", Toast.LENGTH_SHORT).show();
+//                }
+//                else {
+//
+//                    RadioButton radioButton = (RadioButton) radio4.findViewById(ans4);
+//
+//                    Toast.makeText(MainActivity.this, radioButton.getText(), Toast.LENGTH_SHORT).show();
 //                    count++;
 //                }
-                //waise toh isse se ghamta farak nhi padna chaiye but still i am doing;
-                ArrayList<String>al=new ArrayList<>();
-                ArrayList<String>al1 = new ArrayList<>();
+                Toast.makeText(MainActivity.this, String.valueOf(count), Toast.LENGTH_SHORT).show();
+            }
+        });
 
-                    al.add(s1);
-                    al.add(s2);
-                    al.add(s3);
-                    al.add(s4);
-                    al1.add(correctAns1);
-                    al1.add(correctAns2);
-                    al1.add(correctAns3);
-                    al1.add(correctAns4);
-                    for(int i=0;i<al.size();i++){
-                        if(al.get(i).equals(al1.get(i))){
-                            count++;
-                        }
-                    }
-
-                TextView display = new TextView(MainActivity.this);
-                display.setText(count);
-                
-                layout1.addView(display);
-
-                Toast.makeText(getBaseContext(),display.getText(),Toast.LENGTH_LONG).show();
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                radio1.clearCheck();
+                radio2.clearCheck();
+                radio3.clearCheck();
+                radio4.clearCheck();
             }
         });
     }
